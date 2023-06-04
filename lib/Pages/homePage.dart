721478a12970f489ff3049507123a9304fbc3584
Tabithamutinda <tabbithamutinda@gmail.com>
2023-06-04 20:08:05 +0300

    My Loans screen

--- lib/Pages/homePage.dart
+++ lib/Pages/homePage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:nipee/AppStuff/appColors.dart';
+import 'package:nipee/Pages/myLoans.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -47,7 +48,7 @@ class _HomePageState extends State<HomePage> {
             height: 20,
           ),
           _buildSelection(),
-           const SizedBox(
+          const SizedBox(
             height: 12,
           ),
           Padding(
@@ -63,20 +64,21 @@ class _HomePageState extends State<HomePage> {
                     fontWeight: FontWeight.w500,
                   ),
                 ),
-                RichText(text: TextSpan(
-                  children: <TextSpan> [
-                    TextSpan(
-                      text: 'View All',style: GoogleFonts.lato(
-                    fontSize: 15,
-                    color: AppColors.black,
-                    fontWeight: FontWeight.w500,
-                    decoration: TextDecoration.underline
-                  ),
+                RichText(
+                    text: TextSpan(children: <TextSpan>[
+                  TextSpan(
+                      text: 'View All',
+                      style: GoogleFonts.lato(
+                          fontSize: 15,
+                          color: AppColors.black,
+                          fontWeight: FontWeight.w500,
+                          decoration: TextDecoration.underline),
                       recognizer: TapGestureRecognizer()
-                          ..onTap = () {}
-                    )
-                  ]
-                ))
+                        ..onTap = () {
+                          // controller.open;
+                          openTransactionSheet(context);
+                        })
+                ]))
               ],
             ),
           ),
@@ -293,70 +295,191 @@ class _HomePageState extends State<HomePage> {
 
   _buildSelection() {
     return Padding(
-        padding: const EdgeInsets.only(
-            right: 28,
-            left: 28,),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-               Container(
-                            width: MediaQuery.of(context).size.height * 0.2,
-                            height: 70,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(8),
-                                color: AppColors.white),
-                            child: MaterialButton(
-                              onPressed: () {},
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Image.asset('assets/images/pay.png'),
-                                  const SizedBox(
-                                    height: 5,
+      padding: const EdgeInsets.only(
+        right: 28,
+        left: 28,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Container(
+              width: MediaQuery.of(context).size.height * 0.2,
+              height: 70,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  color: AppColors.white),
+              child: MaterialButton(
+                onPressed: () {},
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset('assets/images/pay.png'),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    Center(
+                      child: Text(
+                        'Request',
+                        style: GoogleFonts.lato(
+                          fontSize: 14,
+                          color: AppColors.darkGreen,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              )),
+          Container(
+              width: MediaQuery.of(context).size.height * 0.2,
+              height: 70,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  color: AppColors.darkGreen.withOpacity(0.3)),
+              child: MaterialButton(
+                onPressed: () {
+                  Navigator.of(context)
+                      .push(MaterialPageRoute(builder: (_) => const MyLoans()));
+                },
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'assets/images/loan.png',
+                    ),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    Center(
+                      child: Text(
+                        'My Loans',
+                        style: GoogleFonts.lato(
+                          fontSize: 14,
+                          color: AppColors.darkGreen,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              )),
+        ],
+      ),
+    );
+  }
+
+  void openTransactionSheet(BuildContext context) {
+    showModalBottomSheet(
+        backgroundColor: AppColors.background,
+        elevation: 0,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            top: Radius.circular(20),
+          ),
+        ),
+        isScrollControlled: true,
+        useSafeArea: true,
+        context: context,
+        builder: (BuildContext context) {
+          return Padding(
+            padding: const EdgeInsets.only(top: 5),
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              child: Column(
+                children: [
+                  Container(
+                    height: 2,
+                    width: MediaQuery.of(context).size.width * 0.2,
+                    color: Colors.grey[300], // Color of the top-notch line
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      IconButton(
+                          onPressed: () {},
+                          icon: const Icon(Icons.cancel_sharp)),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    'Your Transactions',
+                    style: GoogleFonts.sourceSansPro(
+                      fontSize: 16,
+                      color: AppColors.darkGreen,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Expanded(
+                    child: ListView(
+                        physics: const AlwaysScrollableScrollPhysics(),
+                        children: List.generate(20, (index) {
+                          return ListTile(
+                            leading: Container(
+                              width: 50,
+                              height: 50,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(100),
+                                  color: AppColors.white),
+                              child: const Icon(Icons.star),
+                            ),
+                            title: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  'QWERTYHGGF',
+                                  style: GoogleFonts.lato(
+                                    fontSize: 14,
+                                    color: AppColors.black,
+                                    fontWeight: FontWeight.w500,
                                   ),
-                                  Center(
-                                    child: Text(
-                                      'Request',
-                                      style: GoogleFonts.lato(
-                                        fontSize: 14,
-                                        color: AppColors.darkGreen,
-                                        fontWeight: FontWeight.w600,
-                                      ),
-                                    ),
-                                  )
-                                ],
-                              ),
-                            )),
-                            Container(
-                            width: MediaQuery.of(context).size.height * 0.2,
-                            height: 70,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(8),
-                                color: AppColors.darkGreen.withOpacity(0.3)),
-                            child: MaterialButton(
-                              onPressed: () {},
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Image.asset('assets/images/loan.png',),
-                                  const SizedBox(
-                                    height: 5,
+                                ),
+                                Text(
+                                  '500.00',
+                                  style: GoogleFonts.lato(
+                                    fontSize: 14,
+                                    color: AppColors.green,
+                                    fontWeight: FontWeight.w400,
                                   ),
-                                  Center(
-                                    child: Text(
-                                      'My Loans',
-                                      style: GoogleFonts.lato(
-                                        fontSize: 14,
-                                        color: AppColors.darkGreen,
-                                        fontWeight: FontWeight.w600,
-                                      ),
-                                    ),
-                                  )
-                                ],
-                              ),
-                            )),
-              ],
+                                )
+                              ],
+                            ),
+                            subtitle: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  '24 May 2023',
+                                  style: GoogleFonts.lato(
+                                    fontSize: 12,
+                                    color: AppColors.subtitle,
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                                ),
+                                Text(
+                                  'Deposit',
+                                  style: GoogleFonts.lato(
+                                    fontSize: 12,
+                                    color: AppColors.darkGreen,
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                                )
+                              ],
+                            ),
+                          );
+                        })),
+                  ),
+                ],
+              ),
             ),
-            );
+          );
+        });
   }
 }
